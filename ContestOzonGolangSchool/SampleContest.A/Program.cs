0ca4cf5eee2fb9b80424d77708659b8565905b26
Eugene <dezmanmail@gmail.com>
2020-05-28 07:19:04 +0700

    Решение задачи A в Пробном тесте

---
+++ ContestOzonGolangSchool/SampleContest.A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SampleContest.A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            var sum = input.Trim().Split(' ').Select(e => int.Parse(e)).Sum();
+            Console.Write(sum);
+        }
+    }
+}
