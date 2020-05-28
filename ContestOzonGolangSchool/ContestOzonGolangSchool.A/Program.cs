6afe2ce3155cd273112910487935c33c373e9ee3
Eugene <dezmanmail@gmail.com>
2020-05-28 07:54:53 +0700

    Задача А
    Добавил генератор данных для задачи

---
+++ ContestOzonGolangSchool/ContestOzonGolangSchool.A/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ContestOzonGolangSchool.A
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+#if DEBUG
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+#endif
+            var input = File.ReadAllText("input-201.txt");
+            var digits = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(e => int.Parse(e));
+            var successPairs = new HashSet<int>();
+            foreach (var digit in digits)
+            {
+                if (successPairs.Contains(digit))
+                {
+                    successPairs.Remove(digit);
+                }
+                else
+                {
+                    successPairs.Add(digit);
+                }
+            }
+#if DEBUG
+            Console.WriteLine(sw.ElapsedMilliseconds);
+#endif
+            Console.Write(successPairs.Count > 0 ? successPairs.First().ToString() : "None");
+        }
+    }
+}
---
+++ ContestOzonGolangSchool/ContestOzonGolangSchool.InputGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ContestOzonGolangSchool.InputGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args.FirstOrDefault()?.ToLower())
+            {
+                case "a":
+                    {
+                        var res = string.Empty;
+                        var random = new Random();
+                        var max = int.Parse(args.Skip(1).FirstOrDefault() ?? "10000");
+                        for (var i = 0; i < max; i++)
+                        {
+                            res += random.Next() + Environment.NewLine;
+                        }
+                        File.WriteAllText("input-201.txt", res);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Key unsupported");
+                        break;
+                    }
+            }
+        }
+    }
+}
