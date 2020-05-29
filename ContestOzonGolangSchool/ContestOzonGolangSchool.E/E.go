515f4052420b2da817cf777e4e068f45e8738341
Eugene <dezmanmail@gmail.com>
2020-05-29 16:16:48 +0700

    [E] Решение с разнесением вычисления суммы и чтения из каналов через goroutine и синхронизацию для сохранения порядка выдачи в out дает невнятную ошибку WA(wrong-answer).
    Экспериментально выявлено:
    - при неверных значениях в out в лог пишет какое число именно не верно
    - при чтении из in каналов больше необходимого, так же пишет об этом в лог
    
    Т е выяснить причину WA не удалось..

--- ContestOzonGolangSchool/ContestOzonGolangSchool.E/E.go
+++ ContestOzonGolangSchool/ContestOzonGolangSchool.E/E.go
@@ -3,140 +3,118 @@ package main
 import (
 	"sync"
 	"time"
+	"math/rand"
+	"fmt"
 )
 
 func Merge2Channels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
+	// println()
 	go Sum2ChannelsN(f, in1, in2, out, n)
 }
 
-func ReadChannel(in <-chan int, f func(v int), wg *sync.WaitGroup) {
-// loopW:
-// 	for {
-// 		select {
-// 		case
-		 res := <-in
-		// :
-			f(res)
-			wg.Done()			
-	// 		return
-	// 	default:
-	// 		time.Sleep(1)
-	// 		break loopW
-	// 	}
-	// }
-}
-
-func GetAdder() func(params ...int) int {
-	sum := int(0)
-	return func(params ...int) (ret int) {
-		for _, num := range params {
-			sum += num
-		}
-		ret = sum
-		return
+func SumChannels(f func(int) int, in1 int, in2 int, out chan<- int, key int, wg *sync.WaitGroup, pwg *sync.WaitGroup) {
+	res := f(in1) + f(in2)
+    if key > 0 {
+		pwg.Wait()
 	}
-}
-
-func SumChannels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int) {
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	adder := GetAdder()
-	proccessor := func(v int) {
-		adder(f(v))
-	}
-	
-	go ReadChannel(in1, proccessor, &wg)
-	go ReadChannel(in2, proccessor, &wg)
-
-	wg.Wait()
-	out <- adder()
+	out <- res
+	defer wg.Done()
+	// println(key, ":", res)
 }
 
 func Sum2ChannelsN(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
-	for i := 0; i < n; i++ {
-		SumChannels(f, in1, in2, out)
+	prevChanWg := new(sync.WaitGroup)
+	prevCalcWg := new(sync.WaitGroup)
+	i := 0
+	for  {
+		currChanWg := new(sync.WaitGroup)
+		currChanWg.Add(1)
+		currCalcWg := new(sync.WaitGroup)
+		currCalcWg.Add(1)
+
+		go func(key int, wg *sync.WaitGroup, pwg *sync.WaitGroup, cwg *sync.WaitGroup, pcwg *sync.WaitGroup) {
+			if key > 0 {
+				pwg.Wait()
+			}
+			x1 := <-in1
+			x2 := <-in2
+			wg.Done()
+			go SumChannels(f, x1, x2, out, key, cwg, pcwg)
+		}(i, currChanWg, prevChanWg, currCalcWg, prevCalcWg)
+		prevCalcWg = currCalcWg
+		prevChanWg = currChanWg
+		i++
 	}
 }
 
 func f(x int) int {
-	time.Sleep(10000)
 	sum := 0
-	for i := 0; i < 10000000; i++ {
-		sum = i
+	max := 10000000
+	for i := 0; i < max; i++ {
+		sum += i
 	}
-	sum -= 1000000
+	sum -= max
+	return f_fast(x)
+}
 
-	return x + 2
+func f_fast(x int) int {
+	return x
 }
 
 func pull(in1 chan<- int, x int, s int) {
 	in1 <- x
 }
 
-func makeEvenGenerator() func() uint {
-    i := uint(0)
-    return func() (ret uint) {
-        ret = i
-        i += 2
-        return
-    }
-}
 func main() {
-    // nextEven := makeEvenGenerator()
-    // println(nextEven()) // 0
-	// println(nextEven()) // 2
-	
-	// nextEven = makeEvenGenerator()
-    // println(nextEven()) // 0
-    // println(nextEven()) // 2
+	max := 10
 
-	in1 := make(chan int, 10)
-	in2 := make(chan int, 10)
-	out := make(chan int, 10)
-	pull(in1, 4, 5)  //in1 <- 4
-	pull(in2, 5, 3)  //in2 <- 5
+	in1 := make(chan int, max)
+	in2 := make(chan int, max)
+	ans := make(chan int, max)
+	out := make(chan int, max)
 
-	pull(in1, 13, 5) //in1 <- 4
-	pull(in2, 7, 3) //in2 <- 5
-
-	pull(in1, 1234243, 5) //in1 <- 4
-	pull(in2, 724243, 3) //in2 <- 5
-
-	pull(in1, 1342433, 5) //in1 <- 4
-	pull(in2, 723232, 3) //in2 <- 5
-
-	pull(in1, 123233, 5) //in1 <- 4
-	pull(in2, 75656, 3) //in2 <- 5
-
-	pull(in1, 13000, 5) //in1 <- 4
-	pull(in2, 65657, 3) //in2 <- 5
-
-	pull(in1, 1300076, 5) //in1 <- 4
-	pull(in2, 6565712, 3) //in2 <- 5
-
-	pull(in1, 1303400, 5) //in1 <- 4
-	pull(in2, 6563457, 3) //in2 <- 5
-
-	Merge2Channels(f, in1, in2, out, 10)
-
-	time.Sleep(1)
-	// var wg sync.WaitGroup
-	// wg.Add(10)
-	println("M:wait")
-// loop:	
-	for {
-		select {
-		case res := <-out:
-			println("M:", res)
-			time.Sleep(2000)
-			// wg.Done()
-			// break loop
-		default:
-			time.Sleep(200)
-			println("M:...")
-			// break loop
-		}
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < max; i++ {
+		x1 := rand.Intn(1000)
+		pull(in1, x1, 5)
+		x2 := rand.Intn(24242)
+		pull(in2, x2, 3)
+		a := f_fast(x2) + f_fast(x1)
+		pull(ans, a, 3)
+		// println("a(", i, "):", a, "=", x1, "+", x2)
 	}
-//	wg.Wait()
+
+	Merge2Channels(f, in1, in2, out, max)
+
+	cnt := 0
+	// println("M:wait")
+	for {
+		
+		// select {
+		// case 
+		res, ok := <-out
+			if !ok {
+				return
+			}
+			cnt++
+			a := <-ans
+			if (a != res) {
+				panic(fmt.Sprintf("%d: %d <> %d", cnt, res, a))
+			}
+			println("M(", cnt,"):", res, "=", a)
+			if cnt >= max {
+				return
+			}	
+		// default:
+			// println("M:...")
+			// rand.Seed(time.Now().UnixNano())
+			// x1 := rand.Intn(1000)
+			// pull(in1, x1, 5)
+			// x2 := rand.Intn(24242)
+			// pull(in2, x2, 3)
+			// a := f_fast(x2) + f_fast(x1)
+			// pull(ans, a, 3)
+			// time.Sleep(2)
+		// }
+	}
 }
