e22d223d986906064cc4acf966c18178a3580b9b
Eugene <dezmanmail@gmail.com>
2020-05-31 12:20:04 +0700

    [E] Вынес syncMap и syncCounter в пакеты

---
+++ ContestOzonGolangSchool/ContestOzonGolangSchool.E/src/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"sync"
+	"time"
+	"math/rand"
+	"fmt"
+	// E "syncMap"
+	E "syncCounter"
+	// "math"
+)
+
+func elapsed(what string) func() {
+    start := time.Now()
+    return func() {
+        fmt.Printf("%s took %v\n", what, time.Since(start))
+    }
+}
+
+func f(x int) int {
+	sum := 0
+	max := 100000000
+	for i := 0; i < max; i++ {
+		sum += i
+	}
+	sum -= max
+	return f_fast(x)
+}
+
+func f_fast(x int) int {
+	return x * x
+}
+
+func pull(in chan<- int, x int, s int) {
+	in <- x
+}
+
+func main() {
+	max := 1000
+
+	defer elapsed(fmt.Sprintf("[syncMap] max: %d", max))()
+
+	// var t sync.Map
+	// t.Store(1,1)
+	// e,ok := t.Load(0)
+	// println(ok, e)
+
+	in1 := make(chan int, max)
+	in2 := make(chan int, max)
+	ans := make(chan int, max)
+	out := make(chan int, max)
+
+	rand.Seed(time.Now().UnixNano())
+	go func() {
+		for i := 0; i < max; i++ {
+			x1 := rand.Intn(1000)
+			pull(in1, x1, 5)
+			x2 := rand.Intn(24242)
+			pull(in2, x2, 3)
+			a := f_fast(x2) + f_fast(x1)
+			pull(ans, a, 3)
+			// println("a(", i, "):", a, "=", x1, "+", x2)
+		}
+	}()
+
+	E.Merge2Channels(f, in1, in2, out, max-3)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		cnt := 0
+		// println("M:wait")
+		for {
+			// println(cnt)
+			if cnt == max - 3 {
+				close(in1)
+				close(in2)
+				println("main closed")
+				return
+			}	
+			
+			select {
+			case res, ok := <-out:
+				if !ok {
+					println("main exit")
+					return
+				}
+				
+				a := <-ans
+				if (a != res) {
+					panic(fmt.Sprintf("%d: %d <> %d", cnt, res, a))
+				}
+				// println("M(", cnt,"):", res, "=", a)
+				cnt++	
+			// default:
+				// println("M:...")
+				// rand.Seed(time.Now().UnixNano())
+				// x1 := rand.Intn(1000)
+				// pull(in1, x1, 5)
+				// x2 := rand.Intn(24242)
+				// pull(in2, x2, 3)
+				// a := f_fast(x2) + f_fast(x1)
+				// pull(ans, a, 3)
+				// time.Sleep(2)
+			}
+		}
+	}()
+	wg.Wait()
+}
--- ContestOzonGolangSchool/ContestOzonGolangSchool.E/src/syncMap/E10.go
+++ ContestOzonGolangSchool/ContestOzonGolangSchool.E/src/syncMap/E10.go
@@ -1,11 +1,7 @@
-package main
+package syncMap
 
 import (
 	"sync"
-	"time"
-	"math/rand"
-	"fmt"
-	// "math"
 )
 
 type syncMap struct{
@@ -37,12 +33,12 @@ func (b *syncMap) Delete(key int) {
 }
 
 func Merge2Channels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
-	f1 := make(chan int, 10)
-	f2 := make(chan int, 10)
+	f1 := make(chan int, 5)
+	f2 := make(chan int, 5)
 	b1 := CreateMap()
 	b2 := CreateMap()
-	s1 := make(chan bool, 10)
-	s2 := make(chan bool, 10)
+	s1 := make(chan bool, 2)
+	s2 := make(chan bool, 2)
 
 	go ChannelToBuff(f, in1, n, b1, s1)
 	go ChannelToBuff(f, in2, n, b2, s2)
@@ -126,91 +122,4 @@ func MapToChanByOrder(b *syncMap, out chan<- int, max int, signal <-chan bool) {
 	close(out)
 }
 
-func f(x int) int {
-	sum := 0
-	max := 100000000
-	for i := 0; i < max; i++ {
-		sum += i
-	}
-	sum -= max
-	return f_fast(x)
-}
 
-func f_fast(x int) int {
-	return x * x
-}
-
-func pull(in chan<- int, x int, s int) {
-	in <- x
-}
-
-func main() {
-	max := 1000
-
-	// var t sync.Map
-	// t.Store(1,1)
-	// e,ok := t.Load(0)
-	// println(ok, e)
-
-	in1 := make(chan int, max)
-	in2 := make(chan int, max)
-	ans := make(chan int, max)
-	out := make(chan int, max)
-
-	// rand.Seed(time.Now().UnixNano())
-	// for i := 0; i < max; i++ {
-	// 	x1 := rand.Intn(1000)
-	// 	pull(in1, x1, 5)
-	// 	x2 := rand.Intn(24242)
-	// 	pull(in2, x2, 3)
-	// 	a := f_fast(x2) + f_fast(x1)
-	// 	pull(ans, a, 3)
-	// 	// println("a(", i, "):", a, "=", x1, "+", x2)
-	// }
-
-	Merge2Channels(f, in1, in2, out, max-3)
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		cnt := 0
-		// println("M:wait")
-		for {
-			// println(cnt)
-			if cnt == max - 3 {
-				close(in1)
-				close(in2)
-				println("main closed")
-				return
-			}	
-			
-			select {
-			case res, ok := <-out:
-				if !ok {
-					println("main exit")
-					return
-				}
-				
-				a := <-ans
-				if (a != res) {
-					panic(fmt.Sprintf("%d: %d <> %d", cnt, res, a))
-				}
-				// println("M(", cnt,"):", res, "=", a)
-				cnt++	
-			default:
-				// println("M:...")
-				rand.Seed(time.Now().UnixNano())
-				x1 := rand.Intn(1000)
-				pull(in1, x1, 5)
-				x2 := rand.Intn(24242)
-				pull(in2, x2, 3)
-				a := f_fast(x2) + f_fast(x1)
-				pull(ans, a, 3)
-				time.Sleep(2)
-			}
-		}
-	}()
-	wg.Wait()
-}
